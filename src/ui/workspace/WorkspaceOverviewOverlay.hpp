99fc5c60cd60f82a1293b9d00a3ac0ed0aff31f6
Zahed <kzahed610@gmail.com>
2026-08-06 18:50:24 +0530

    Refined outline animation and minor inconsistency

--- src/ui/workspace/WorkspaceOverviewOverlay.hpp
+++ src/ui/workspace/WorkspaceOverviewOverlay.hpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <functional>
 #include <gtk/gtk.h>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -102,7 +103,11 @@ private:
     void handle_drag_update(double offset_x, double offset_y);
     void handle_drag_end(double offset_x, double offset_y);
     void reset_drag() noexcept;
-    void select_realm(int index);
+    void select_realm(int index, int preferred_card_index = -2);
+    void transition_selection(int realm_index, int card_index);
+    [[nodiscard]] std::optional<graphene_rect_t>
+        selected_card_outline_bounds() const;
+    void finish_selection_transition() noexcept;
     void stop_animation(bool snap_to_target) noexcept;
     void prepare_card_transition(const WorkspaceOverviewState& next);
     void finish_card_transition() noexcept;
@@ -148,9 +153,15 @@ private:
     guint animation_tick_id_ = 0;
     gint64 animation_start_time_us_ = 0;
     gint64 card_animation_start_time_us_ = 0;
+    gint64 selection_animation_start_time_us_ = 0;
+    graphene_rect_t selection_outline_start_bounds_{};
     double card_animation_progress_ = 1.0;
+    double selection_animation_progress_ = 1.0;
+    double selection_outline_start_opacity_ = 0.0;
     bool realm_animation_active_ = false;
     bool card_animation_active_ = false;
+    bool selection_animation_active_ = false;
+    bool selection_outline_has_start_bounds_ = false;
     bool assets_attempted_ = false;
 };
 
